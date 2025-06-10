2d26589d8d10730943426549ebad2bf2da32a3b9
Bigyan1000 <bigyanbhattarai10@gmail.com>
2025-06-10 20:54:43 +0545

    variables declaration

---
+++ hello.py
@@ -0,0 +1,25 @@
+
+firstName = "Bigyan"
+
+lastName = "Bhattarai"
+
+
+fullName = firstName  + " " + lastName
+
+print("hello from ", fullName)
+
+greetings = "My name is bigyan"
+
+x = 3
+y =2
+
+
+def sum(x,y):
+    return x + y
+
+result = sum(5,2)
+
+print("the sum of two number", sum(x,y))
+print("the sum of 5 and 2 is", result)
+
+print(greetings)
