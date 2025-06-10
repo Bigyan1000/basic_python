18b4fd1ac78cbcedbf5a37f42ea31a9ba39e3f85
Bigyan1000 <bigyanbhattarai10@gmail.com>
2025-06-10 21:16:43 +0545

    separated function to their own file

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+from mathFunctions import sum
+from mathFunctions import multiply
 
 firstName = "Bigyan"
 
@@ -14,15 +16,12 @@ x = 3
 y =2
 
 
-def sum(x,y):
-    return x + y
-def multiply(x,y):
-    return x*y
+
 
 product= multiply(3,2)
 print(product)
 
-result = sum(5,2)
+result = sum(x,y)
 
 print("the sum of two number", sum(x,y))
 print("the sum of 5 and 2 is", result)
---
+++ mathFunctions.py
@@ -0,0 +1,9 @@
+def sum(x,y):
+    return x + y
+
+def multiply(x,y):
+    return x*y
+
+
+def difference(x,y):
+    return x -y
